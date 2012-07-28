2ae40ce467a6eb062f47c3db6872d31c5d2c794d
weed <weed_7777@yahoo.co.jp>
2012-07-28 14:35:17 +0900

    Initial Commit

---
+++ GetOgImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GetOgImage
+//
+//  Created by 達郎 植田 on 12/07/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GetOgImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GetOgImage
+//
+//  Created by 達郎 植田 on 12/07/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
