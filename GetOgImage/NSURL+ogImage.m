91c8354e2a77062703b492df8ba81fec009babe0
weed <weed_7777@yahoo.co.jp>
2012-08-01 08:56:24 +0900

    before create library

---
+++ GetOgImage/NSURL+ogImage.m
@@ -0,0 +1,13 @@
+//
+//  NSURL+ogImage.m
+//  GetOgImage
+//
+//  Created by 達郎 植田 on 12/08/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSURL+ogImage.h"
+
+@implementation NSURL(ogImage)
+
+@end
